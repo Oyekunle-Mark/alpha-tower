c9f4a62f70420d3dacbae26f784e44656fe4f6ef
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-27 23:23:22 +0100

    Fetches repos by stars

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+GITHUB_API_URL = "https://api.github.com/search/repositories"
+
+
+def get_repos_with_most_stars():
+    parameters = {"q": "stars:>200000"}
+
+    response = requests.get(GITHUB_API_URL, params=parameters)
+
+    return response.json()
+
+
+print(get_repos_with_most_stars())
