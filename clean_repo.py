9e18d49b69c6594c96c3a13f79f0db91336f37d0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-28 00:33:43 +0100

    Documented the file

--- clean_repo.py
+++ clean_repo.py
@@ -1,4 +1,14 @@
 def get_useful_fields(repos, repo_count):
+    """Selects the useful fields from the repos
+
+    Arguments:
+        repos {list} -- the list of repos to be cleaned
+        repo_count {int} -- the number of repos
+
+    Returns:
+        {dict} -- the standard output format for the program
+    """
+
     filtered_repos = []
 
     for repo in repos:
