b06036bf888eba4c502144315cf5707aaf98e8ce
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-28 00:34:02 +0100

    A thorough documentation

--- get_repos.py
+++ get_repos.py
@@ -5,6 +5,23 @@ GITHUB_API_URL = "https://api.github.com/search/repositories"
 
 
 def get_repos_with_most_stars(languages, sort="stars", order="desc", stars=50000):
+    """Fetches the data from the GitHub API
+
+    Arguments:
+        languages {list} -- the list of languages 
+
+    Keyword Arguments:
+        sort {str} -- the sort condition (default: {"stars"})
+        order {str} -- the order pattern (default: {"desc"})
+        stars {int} -- the minimum number of stars (default: {50000})
+
+    Raises:
+        RuntimeError: Error that might occur while fetching data from the API
+
+    Returns:
+        {tuple} -- the repos and number of repos
+    """
+
     parameters = build_parameters(languages, sort, order, stars)
 
     response = get(GITHUB_API_URL, params=parameters)
