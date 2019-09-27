9b5e98f07c0bbb652266016d933f9f5be00ef545
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-27 23:52:55 +0100

    Builds the userful fields from the input argument

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def create_query(languages, stars):
 
     return query
 
+
 def build_parameters(languages, sort, order, stars):
-    query = create_query(languages, stars);
+    query = create_query(languages, stars)
 
     return {"q": query, "sort": sort, "order": order}
-    
+
 
 def get_repos_with_most_stars(languages, sort="stars", order="desc", stars=100000):
     parameters = build_parameters(languages, sort, order, stars)
@@ -23,7 +24,24 @@ def get_repos_with_most_stars(languages, sort="stars", order="desc", stars=10000
     response = requests.get(GITHUB_API_URL, params=parameters)
 
     repos = response.json()["items"]
-    return repos
+    repo_count = response.json()["total_count"]
+
+    return repos, repo_count
+
+
+def get_useful_fields(repos):
+    filtered_repos = []
+
+    for repo in repos:
+        filtered_repos.append({
+            "language": repo["language"],
+            "stars": repo["stargazers_count"],
+            "name": repo["name"],
+            "forks": repo["forks_count"],
+            "issues": repo["open_issues_count"]
+        })
+
+    return filtered_repos
 
 
 if __name__ == "__main__":
