f41919871846e6ad6d042e4356999cb7c4efceff
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-28 00:34:19 +0100

    Added function documentaion

--- parameters.py
+++ parameters.py
@@ -1,4 +1,14 @@
 def create_query(languages, stars):
+    """Builds the query string
+
+    Arguments:
+        languages {list} -- the list of languages
+        stars {int} -- the minimum number of stars
+
+    Returns:
+        {str} -- the built query string
+    """
+
     query = f"stars:>{stars} "
 
     for language in languages:
@@ -8,6 +18,17 @@ def create_query(languages, stars):
 
 
 def build_parameters(languages, sort, order, stars):
+    """Builds the request parameters
+
+    Arguments:
+        languages {list} -- the list of languages
+        sort {str} -- the sort condition
+        order {str} -- the order pattern
+        stars {int} -- the minimum number of stars
+
+    Returns:
+        {str} -- the built query string
+    """
     query = create_query(languages, stars)
 
     return {"q": query, "sort": sort, "order": order}
