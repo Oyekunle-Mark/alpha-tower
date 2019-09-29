46d2e96553909388432ff28f2caf74a359538f5e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-29 19:29:29 +0100

    Added another assertion

--- test_parameters.py
+++ test_parameters.py
@@ -10,6 +10,9 @@ class ParametersTest(unittest.TestCase):
         self.assertEqual(create_query(test_languages, test_stars),
                          f"stars:>{test_stars} language:{test_languages[0]} ", "should equal")
 
+        self.assertNotEqual(create_query(test_languages, test_stars),
+                            f"stars:>{test_stars} language:{test_languages} ", "should not equal")
+
 
 if __name__ == '__main__':
     unittest.main()
